f637e3c81a6b675b7803707c04ca095b9b763c1b
DiegoMutre <tysonmutre10@gmail.com>
2021-04-04 12:32:20 -0500

    Feat: Return a promise in showMenu and puase

--- helpers/messages.js
+++ helpers/messages.js
@@ -1,49 +1,57 @@
 require('colors');
 
 const showMenu = () => {
-    console.clear();
 
-    // Show question
-    console.log('========================='.green);
-    console.log('  Select an option  '.green);
-    console.log('=========================\n'.green);
+    return new Promise(resolve => {
 
-    // Show options
-    console.log(`${'1.'.green} Create Task`);
-    console.log(`${'2.'.green} List Tasks`);
-    console.log(`${'3.'.green} List Completed Tasks`);
-    console.log(`${'4.'.green} List pending Tasks`);
-    console.log(`${'5.'.green} Complete Task(s)`);
-    console.log(`${'6.'.green} Delete a Tasks`);
-    console.log(`${'0.'.green} Leave \n`);
+        console.clear();
 
-    /*
+        // Show question
+        console.log('========================='.green);
+        console.log('  Select an option  '.green);
+        console.log('=========================\n'.green);
+
+        // Show options
+        console.log(`${'1.'.green} Create Task`);
+        console.log(`${'2.'.green} List Tasks`);
+        console.log(`${'3.'.green} List Completed Tasks`);
+        console.log(`${'4.'.green} List pending Tasks`);
+        console.log(`${'5.'.green} Complete Task(s)`);
+        console.log(`${'6.'.green} Delete a Tasks`);
+        console.log(`${'0.'.green} Leave \n`);
+
+        /*
         Readline is a node module which allows us to receive and send messages through the console
         *input : means that you will receive a entry.
         *output : means that you will send an output message
-    */
-    const readline = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+        */
+       const readline = require('readline').createInterface({
+           input: process.stdin,
+           output: process.stdout
+        });
 
-    /*
+        /*
         *opt : Opt contains the message sent by the user
-    */
-    readline.question('Select an option: ', opt => {
-        readline.close(); // Close the terminal
+        */
+       readline.question('Select an option: ', opt => {
+           readline.close(); // Close the terminal
+           resolve(opt);
+        });
     });
 };
 
 
 const pause = () => {
-    const readline = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+    return new Promise(resolve => {
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
 
-    readline.question(`\nPress ${ 'ENTER'.green } to continue\n`, () => {
-        readline.close();
+        readline.question(`\nPress ${ 'ENTER'.green } to continue\n`, () => {
+            readline.close();
+            resolve();
+        });
     });
 }
 
