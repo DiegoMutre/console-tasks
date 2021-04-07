a7b8671b88848fce6058b936a337db4cee45a5ba
DiegoMutre <tysonmutre10@gmail.com>
2021-04-06 20:12:59 -0500

    Feat: Use Tasks model to save the task

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 require("colors");
 const { inquirerMenu, pause, readInput } = require("./helpers/inquirer");
+const Tasks = require("./models/Tasks");
 
 console.clear();
 
 const main = async () => {
     let opt = "";
+    const tasks = new Tasks();
 
     do {
         // Print the menu
         opt = await inquirerMenu();
 
         if (opt === "1") {
-            // TODO: Save the desc in our fake DB
             const desc = await readInput("Description:");
+            tasks.createTask(desc);
         }
+        await pause();
     } while (opt !== "0");
 };
 
