e875adee414652e578ac67f71b18085c853b6c5e
DiegoMutre <tysonmutre10@gmail.com>
2021-04-07 19:40:28 -0500

    Feat: Create arrayList getter

--- models/Tasks.js
+++ models/Tasks.js
@@ -3,6 +3,15 @@ const Task = require("./Task");
 class Tasks {
     _list = {};
 
+    get arrayList() {
+        const list = [];
+        Object.keys(this._list).forEach(key => {
+            const task = this._list[key];
+            list.push(task);
+        });
+        return list;
+    }
+
     constructor() {
         this._list = {};
     }
