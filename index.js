1f1d868ca4565d48999d27b9f71f3659ff1fe691
DiegoMutre <tysonmutre10@gmail.com>
2021-04-09 06:40:45 -0500

    Feat: Show the tasks from the DB in the option 2
    Now when the option 2 is selected, will load the
    data from the db.json file

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 require("colors");
 const { inquirerMenu, pause, readInput } = require("./helpers/inquirer");
 const Tasks = require("./models/Tasks");
-const { saveInTheDB } = require("./utils/saveFile");
+const { saveInTheDB, readDB } = require("./utils/saveFile");
 
 console.clear();
 
 const main = async () => {
     let opt = "";
     const tasks = new Tasks();
+    const tasksFromTheDB = readDB();
+
+    if (tasksFromTheDB) {
+        tasks.loadTasksFromDB(tasksFromTheDB);
+    }
 
     do {
         // Print the menu
@@ -16,6 +21,10 @@ const main = async () => {
         if (opt === "1") {
             const desc = await readInput("Description:");
             tasks.createTask(desc);
+        }
+
+        if (opt === "2") {
+            tasks.showTasks();
         }
 
         saveInTheDB(tasks.arrayList);
